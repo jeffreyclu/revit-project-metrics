d45e5383de00167466005cf92f715ee146b914a4
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-08-18 16:49:53 -0400

    prettier test

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import './app.styles.css';
+import React, { useState, useEffect } from "react";
+import "./app.styles.css";
 
 const App = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      let resp = await fetch('http://localhost:3001');
+      let resp = await fetch("/app");
+      console.log(resp);
       let data = await resp.json();
       console.log(data);
-    }
+    };
     fetchData();
-  }, [])
-  return(
+  }, []);
+  return (
     <div>
       <h1>sup</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+
+app.get('/app', (req, res) => {
+  res.send({msg: 'Hello World!'})
 });
 
 app.listen(PORT, () => {
