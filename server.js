ef91830ae7ec34d0ec6248095d4d035170287992
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-08-18 17:29:03 -0400

    edited procfile

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+app.set('port', (process.env.PORT || 3001));
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.get('/app', (req, res) => {
   res.send({msg: 'Hello World!'})
 });
 
-app.listen(PORT, () => {
-  console.log(`server running at port ${PORT}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server running at port ${app.get('port')}`);
 })
