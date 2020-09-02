468b7452192226b172cfb622d6bd70124dade0fa
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-09-02 16:29:59 -0400

    fixes .env key

--- server/models/db-model.js
+++ server/models/db-model.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // create a new pool here using the connection string above
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.LOCAL_DATABASE_URL
 });
 
 module.exports = {
